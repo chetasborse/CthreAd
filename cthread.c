b868da755464ce1f80a635b87a2f56264938d914
Aniket Jayateerth <jayateerth.aniket@gmail.com>
2021-04-07 14:57:22 +0530

    feat: implemented queue structure of many-one thread model

--- cthread.c
+++ cthread.c
@@ -1,83 +1,78 @@
-#define _GNU_SOURCE
-#include<sched.h>
-#include <stdlib.h>
+#include <ucontext.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <setjmp.h>
-#include <sys/wait.h>
-#include<signal.h>
-#include <linux/futex.h>
-#include <syscall.h>
+#include <stdlib.h>
 #include "cthread.h"
 
-#define SIZESTACK (1024 * 1024)
+int thread_id = 1;
 
-void handle_usr1(int sig) {
-    fflush(stdout);
-}
-
-// Installs signal handler for SIGUSR1 which is used to receive termination signal
-void cthread_init() {
-    struct sigaction sa;
-    sa.sa_handler = &handle_usr1;
-    sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-    sigaction(SIGUSR1, &sa, NULL);
-}
-
-// Runs the function passed to the thread
-int cthread_run(void *c) {
-    cthread *c1 = (cthread *)c;
-    if(sigsetjmp(c1->env, 0) == 0) {
-        c1->result = c1->func(c1->args);
+thread_queue *q_init()
+{
+    thread_queue *q;
+    q = (thread_queue *)malloc(sizeof(thread_queue));
+    if (!q)
+    {
+        return NULL;
     }
-    kill(c1->ptid, SIGUSR1);
-    c1->execution = 1;
+
+    return q;
+}
+
+int is_qempty(thread_queue *q)
+{
+    return !q->size;
+}
+
+cthread *running;
+cthread *main;
+ucontext_t *main_context;
+
+void enqueue(thread_queue *q, cthread *thread)
+{
+    node *new = (node *)malloc(sizeof(node));
+    new->thread = thread;
+    new->next = NULL;
+
+    if (q->tail != NULL)
+        q->tail->next = new;
+
+    q->tail = new;
+
+    if (q->head == NULL)
+        q->head = q->tail;
+}
+
+cthread *dequeue(thread_queue *q)
+{
+    if (q->head == NULL)
+        return NULL;
+
+    cthread *temp = q->head->thread;
+    if (q->head == q->tail)
+        q->head = q->tail = NULL;
+
+    else
+        q->head = q->head->next;
+
+    return temp;
+}
+
+int exists_in_q(thread_queue *q, cthread *thread)
+{
+    if (is_qempty(q))
+        return 0;
+
+    node *temp = q->head;
+    while (temp)
+    {
+        if (temp->thread == thread)
+            return 1;
+        temp = temp->next;
+    }
+
     return 0;
 }
 
-//Creates a thread for the passed function
-int cthread_create(cthread *c, void *(*f)(void *), void *args) {
-    char *stack, *stackhead;
-    stack = (char *)malloc(SIZESTACK);
+thread_queue *ready;
+thread_queue *blocked;
 
-    if(!stack) {
-		fprintf(stderr, "Unalbe to allocate stack.\n");
-		exit(1);
-	}
-
-    stackhead = stack + SIZESTACK - 1;
-
-    c->execution = 0;
-    c->stack_start = stackhead;
-    c->args = args;
-    c->func = f;
-    c->stack_size = SIZESTACK;
-    c->ptid = getpid();
-   
-    c->tid = clone(cthread_run, stackhead, CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SYSVSEM
-                       | CLONE_SIGHAND | CLONE_THREAD
-                       | CLONE_PARENT_SETTID
-                       | CLONE_CHILD_CLEARTID, c);
-    
-    if(c->tid == -1) {
-		fprintf(stderr, "Unable to clone\n");
-		free(stack);
-		exit(1);
-	}   
-
-    return c->tid;
-}
-
-//Waits for the thread to complete its execution
-int cthread_join(cthread *c, void **result) {
-    while(c->execution == 0)
-        pause();
-
-    if(result != NULL)
-        *result = c->result;
-
-    free((void *)(c->stack_start + 1 - SIZESTACK));
-    return 0;
-}
+void cthread_init()
--- cthread.h
+++ cthread.h
@@ -1,22 +1,72 @@
 #define _GNU_SOURCE
-#include<sched.h>
+#include <sched.h>
 #include <linux/types.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <setjmp.h>
+#include <ucontext.h>
 
-typedef struct cthread {
-    int tid; //thread id
-    int execution; // 1 if the execution of thread completes, 0 if still executing
-    int ptid; //pid of the calling process
-    void *stack_start; //address of the start of the stack
-    int stack_size;
-    void *args; //Pointer pointing to arguments passed to the function
-    void *result; //Pointer pointing to return value of the function
-    void *(*func)(void *); //Pointer pointing to the address of the function called
-    sigjmp_buf env; //Saves current PC and SP when sigsetjmp is called
-}cthread;
+// queue structure for thread pool
+typedef struct thread_queue
+{
+    node *head;
+    node *tail;
+    int size;
+} thread_queue;
+
+// tcb of thread
+typedef struct cthread
+{
+    int tid;                   //thread id
+    int execution;             // 1 if the execution of thread completes, 0 if still executing
+    int ptid;                  //pid of the calling process
+    void *stack_start;         //address of the start of the stack
+    int stack_size;            // size of the stack
+    void *args;                //Pointer pointing to arguments passed to the function
+    void *result;              //Pointer pointing to return value of the function
+    void *(*func)(void *);     //Pointer pointing to the address of the function called
+    sigjmp_buf env;            //Saves current PC and SP when sigsetjmp is called
+    int state;                 // state of the thread
+    ucontext_t context;        // context of thread
+    thread_queue *child_queue; // points to child queue
+
+} cthread;
+
+// node in a queue
+typedef struct node
+{
+    cthread *thread;
+    struct node *next;
+} node;
+
+// states of any thread
+typedef enum state
+{
+    READY,
+    RUNNING,
+    SCHEDULED,
+    WAITING,
+    TERMINATED
+} state;
+
+// structure for scheduler
+typedef struct scheduler
+{
+    thread_queue *main_queue;
+    thread_queue *wait;
+    cthread *main_thread;
+    cthread *current;
+
+    int priority_list[5];
+    int count;
+} scheduler;
+
+typedef struct Semaphore
+{
+    int val;
+    thread_queue *sem_queue;
+} Semaphore;
 
 void cthread_init();
 int cthread_run(void *);
@@ -26,4 +76,3 @@ int cthread_join(cthread *, void **);
 void cthread_exit();
 
 int cthread_get_self();
-
